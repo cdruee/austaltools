1116ed1230e15ae0b06795cb9c05634b077f0ed8
Clemens Druee <druee@uni-trier.de>
2024-01-03 12:11:29 +0100

    fixed sphinx imports again in .gitlab-ci.yml bump fix level

--- austaltools/_dispersion.py
+++ austaltools/_dispersion.py
@@ -14,10 +14,11 @@ logger = logging.getLogger(__name__)
 
 # ----------------------------------------------------
 
-kappa = m.constants.kappa
-gn = m.constants.gn
-_check = m._utils._check
-_isscalar = pd.api.types.is_scalar
+if os.environ.get('BUILDING_SPHINX', 'false') == 'false':
+    kappa = m.constants.kappa
+    gn = m.constants.gn
+    _check = m._utils._check
+    _isscalar = pd.api.types.is_scalar
 
 # =============================================================================
 
