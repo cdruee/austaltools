6321e0a0fff77073f6e6af84ddbcf53051a0259a
Clemens Druee <druee@uni-trier.de>
2024-01-03 02:38:15 +0100

    added gitlab pages to .gitlab-ci.yml bump fix level

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
+# If true, `to do` and `tod oList` produce output, else they produce nothing.
 todo_include_todos = False
 
 
