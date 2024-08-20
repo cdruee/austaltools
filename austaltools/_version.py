24b074c52a49acd856111d14e3298a96dfb00e11
Clemens Druee <druee@uni-trier.de>
2024-08-20 18:50:58 +0200

    dataset_definitions:   - changed (removed "resolution") DGM10-DE bump dev version

--- austaltools/_version.py
+++ austaltools/_version.py
@@ -3,7 +3,7 @@
 __title__ = 'austaltools'
 __description__ = 'Tools for use with austal.'
 __url__ = ''
-__version__ = '0.23.0.dev63'
+__version__ = '0.23.0.dev64'
 __author__ = u'Clemens Drüe'
 __author_email__ = 'druee@uni-trier.de'
 __license__ = 'EUPL-1.2'
