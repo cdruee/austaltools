f9276620e1aae8b0b45f006b1c82157f577a7f30
Clemens Druee <druee@uni-trier.de>
2024-09-15 19:09:50 +0200

    change CI bump major version bump fix level

--- austaltools/_version.py
+++ austaltools/_version.py
@@ -3,7 +3,7 @@
 __title__ = 'austaltools'
 __description__ = 'Tools for use with austal.'
 __url__ = ''
-__version__ = '2.0.0.dev75'
+__version__ = '2.0.1'
 __author__ = u'Clemens Drüe'
 __author_email__ = 'druee@uni-trier.de'
 __license__ = 'EUPL-1.2'
