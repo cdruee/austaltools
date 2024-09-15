1b251d9208c6a535f338632fbab069b42e72c7a8
Clemens Druee <druee@uni-trier.de>
2024-09-16 00:11:26 +0200

    configure_austaltools   fixed TEMP dir bump fix level

--- austaltools/_version.py
+++ austaltools/_version.py
@@ -3,7 +3,7 @@
 __title__ = 'austaltools'
 __description__ = 'Tools for use with austal.'
 __url__ = ''
-__version__ = '2.0.7'
+__version__ = '2.0.8'
 __author__ = u'Clemens Drüe'
 __author_email__ = 'druee@uni-trier.de'
 __license__ = 'EUPL-1.2'
