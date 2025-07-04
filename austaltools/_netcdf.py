482b513633c9115492469828072b209d2e756b86
Clemens Druee <druee@uni-trier.de>
2025-07-04 19:21:11 +0200

    fixed error building documentation on gitlab bump fix level

--- austaltools/_netcdf.py
+++ austaltools/_netcdf.py
@@ -6,9 +6,27 @@ import itertools
 import logging
 import os
 
-import netCDF4
 if os.getenv('BUILDING_SPHINX', 'false') == 'false':
+    import netCDF4
     import numpy as np
+else:
+    # Minimal dummy classes for Sphinx documentation builds
+    class _DummyDataset:
+        """Dummy Dataset class for Sphinx builds."""
+        pass
+
+    class _DummyVariable:
+        """Dummy Variable class for Sphinx builds."""
+        pass
+
+    # Create a mock netCDF4 module
+    class _MockNetCDF4:
+        Dataset = _DummyDataset
+        Variable = _DummyVariable
+
+    # Make the mock module available
+    netCDF4 = _MockNetCDF4()
+
 
 try:
     from . import _storage
